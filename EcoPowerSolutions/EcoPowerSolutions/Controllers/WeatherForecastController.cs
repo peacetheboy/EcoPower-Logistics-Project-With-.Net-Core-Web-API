fa0fa8a89b8f0375062bbbbc4d3680e3fa794fe9
Pk Dagada <36564567@mynwu.ac.za>
2023-08-31 09:11:15 +0100

    Migration

---
+++ EcoPowerSolutions/EcoPowerSolutions/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoPowerSolutions.Controllers
+{
+
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+    }
+}
